325cc18ded636253cc4bada7562f410ff62f6a06
David Urbano <david.urbano_code@outlook.com>
2022-10-25 04:58:11 -0500

    Footer ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import PageButtons from './components/PageButtons';
+import Footer from './components/Footer';
 
 function App() {
 	return (
 		<div className='App'>
 			<PageButtons name='previous' />
 			<PageButtons name='next' />
+			<Footer />
 		</div>
 	);
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import './../styles/Footer.css';
+
+const Footer = () => {
+	return (
+		<>
+			<footer class='Footer'>
+				<span className='Footer-text'>MADE BY David Urbano</span>
+				<img
+					src='https://raw.githubusercontent.com/David-UM/ICPNA-FP-INTERMEDIATE6/main/src/assets/TheBear.png'
+					alt='Bear logo'
+					class='Footer-logo'
+				/>
+				<span className='Footer-information'>ICPNA - INTERMEDIATE 6</span>
+			</footer>
+		</>
+	);
+};
+
+export default Footer;
