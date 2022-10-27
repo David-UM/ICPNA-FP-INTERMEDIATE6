d7eca81cc10ddde81b6a51bf87578315812403b2
David Urbano <david.urbano_code@outlook.com>
2022-10-27 13:30:57 -0500

    Cambiando imagenes

--- src/data/data.js
+++ src/data/data.js
@@ -18,17 +18,17 @@ const data = [
 		page: 2,
 		tittle: 'Why?',
 		images: [
-			'https://htxt002.azurewebsites.net/wp-content/uploads/2018/07/Kurzgesagt-Plastic-Header.png',
-			'https://preview.redd.it/j465vuwu70h61.png?width=640&crop=smart&auto=webp&s=b19b10314b8de6a477e124db937acf9f3f8a4c50',
-			'https://i.gzn.jp/img/2020/09/30/too-late-stop-climate-change/0035.jpg',
+			'https://i.gzn.jp/img/2020/09/30/too-late-stop-climate-change/00.jpg',
+			'https://wordlesstech.com/wp-content/uploads/2016/12/Overpopulation-%E2%80%93-The-Human-Explosion-explained-1.jpg',
+			'https://i.pinimg.com/originals/8a/6f/c3/8a6fc3b46e40f69a520145d81f0e71e6.png',
 		],
 	},
 	{
 		tittle: 'Conclusion',
 		images: [
-			'https://miro.medium.com/max/1400/1*XtbGwZ63Wk3pPCqvF7V6Tg.jpeg',
-			'https://mir-s3-cdn-cf.behance.net/projects/404/233b3776151497.Y3JvcCw4NjIsNjc1LDE2NCww.png',
-			'https://mir-s3-cdn-cf.behance.net/project_modules/fs/a75dc048610661.589d9674a40a0.png',
+			'https://pbs.twimg.com/media/DfqfcLkW0AAeowC.jpg:large',
+			'https://pbs.twimg.com/ext_tw_video_thumb/1425819867538542605/pu/img/NLBNGbMaveG8KF55.jpg:large',
+			'https://pbs.twimg.com/media/ElbcGkoWoAAUUl5.jpg',
 		],
 	},
 ];
